f27dd9b87f438f5f0b335facb427a303612dd912
allthepreaching <admin@allthepreaching.com>
2023-09-22 16:20:25 -0700

    Update

--- header.php
+++ header.php
@@ -49,8 +49,6 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
 
-
-
     <!-- Title -->
     <title>Pastor Anderson Sermons</title>
 
